413721b6070384923e0938c307f0bdb6c8a5c519
Victor Ottati <veoa17@gmail.com>
2018-10-15 22:45:38 +0100

    almost there

--- tester/crawler.py
+++ tester/crawler.py
@@ -1,5 +1,7 @@
 import csv
+import queue
 import logging
+import re
 
 from bs4 import BeautifulSoup
 from collections import deque
@@ -7,7 +9,10 @@ from pyvirtualdisplay import Display
 from selenium import webdriver
 from urllib.parse import urldefrag, urljoin
 
-from exceptions import ImproperlyConfigured 
+from exceptions import (
+    ImproperlyConfigured,
+    EmptyWebPage,
+)
 
 
 class Crawler(object):
@@ -15,23 +20,24 @@ class Crawler(object):
     A Crawler that finds documents that have certain 
     characteristics.
     """
+    allow_external_urls = False
 
-    def __init__(self, db, base_url=None, start_url=None, levels=1):
+    def __init__(self, db, base_url=None, initial_path=None, levels=1):
         """Setup and validate arguments to make them available 
         
         Arguments:
             db  -- MongoDB connection
         
         Keyword Arguments:
-            start_url {str} -- URL (default: {None})
+            initial_path {str} -- URL (default: {None})
             base_url {str} -- URL (default: {None})
             levels {int} -- Number of levels to crawl (default: {1})
         """
 
         if not db:
             raise(ImproperlyConfigured('database connection'))
-        elif not start_url:
-            raise(ImproperlyConfigured('start url'))
+        elif not initial_path:
+            raise(ImproperlyConfigured('initial path'))
         elif not base_url:
             raise(ImproperlyConfigured('base url'))
         elif not int(levels):
@@ -42,61 +48,111 @@ class Crawler(object):
         self.display.start()
         # Add path to your Firefox.
         self.browser = webdriver.Firefox(log_path='/var/log/geckolog.log')
-        # The start might change after the setup method 
-        self.start = start_url
+        # The initial_path might change after the setup method 
+        self.initial_path = initial_path
+        self.base_url = base_url
+        # Queue of urls to visit
+        self.queue = self.get_queue() 
+        # Visited Urls
+        self.visited = self.get_visited()
+        self.url_regex = self.get_url_regex() 
         self._setup()
+        self.broken_urls = self.get_broken_urls()
+    
+    def get_broken_urls(self):
+        return(set())
+    
+    def get_url_regex(self):
+        # Horrible 80 + LINE
+        return(re.compile(r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$'))
+
+    def is_relative(self, url):
+        return not self.url_regex.match(url)
+    
+    def get_base_url(self, url):
+        # Horrible 80 + LINE 2
+        regex = re.compile(r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}$')
+        regex.match(url)
+        return(regex.group())
+
+    def get_queue(self):
+        return(queue.Queue())
+
+    def get_visited(self):
+        return(set())
 
     def _setup(self):
         """
-        Method dedicated to take care of authentication
+        Method dedicated to take care of authentication,
         and general setup to start the crawling
+        """
+        initial_path = self.initial_path
+        base_url = self.base_url
+        if self.is_relative(initial_path):
+            _url = '{}{}'.format(base_url, initial_path)
+        self.queue.put(_url)
+
+    def _process_page_object(self, page_object):
+        """
+        Method dedicated to take care of processing
+        a web page content to do secondary effects.
         """
         pass
     
-    def get_page(self, url):
+    def get_page_source(self, url):
         try:
             self.browser.get(url)
             return self.browser.page_source
         except Exception as e:
+            self.broken_urls.add(url)
             logging.exception(e)
             return
 
-    def get_soup(self, html):
-        if html is not None:
-            soup = BeautifulSoup(html, 'lxml')
-            return soup
-        else:
-            return
-    
-    def get_links(self, soup):
-        for link in soup.find_all('a', href=True): #All links which have a href element
-            link = link['href'] #The actually href element of the link
-            url = urljoin(self.base, urldefrag(link)[0]) #Resolve relative links using base and urldefrag
-            if url not in self.url_queue and url not in self.crawled_urls: #Check if link is in queue or already crawled
-                if url.startswith(self.base): #If the URL belongs to the same domain
-                    self.url_queue.append(url) #Add the URL to our queue
-
-    def get_data(self, soup):
-        try:
-            title = soup.find('title').get_text().strip().replace('\n','')
-        except:
-            title = None
- 
-        return title
-
-    def run_crawler(self):
-        try:
-            while len(self.url_queue): #If we have URLs to crawl - we crawl
-                current_url = self.url_queue.popleft() #We grab a URL from the left of the list
-                self.crawled_urls.append(current_url) #We then add this URL to our crawled list
-                html = self.get_page(current_url) 
-                if self.browser.current_url != current_url: #If the end URL is different from requested URL - add URL to crawled list
-                    self.crawled_urls.append(current_url)
-                soup = self.get_soup(html)
-                if soup is not None:  #If we have soup - parse and write to our csv file
-                    self.get_links(soup)
-                    title = self.get_data(soup)
-                    self.csv_output(current_url, title)
-        except:
-            self.display.stop()
+    def _get_page_object(self, html):
+        if not html:
+            raise(EmptyWebPage)
+        return BeautifulSoup(html, 'lxml')
         
+    def get_and_process_links(self, page, parent_url):
+        links = page.find_all('a', href=True)
+        for link in links:
+            _url = link['href']
+            if(self.is_relative(_url)):
+                _url = '{}{}'.format(self.get_base_url(parent_url), _url)
+            if _url not in self.visited and _url not in self.broken_urls:
+                self.queue.put(_url)
+    
+    def run(self):
+        try:
+            parent_url = '' 
+            while True:
+                _url = self.queue.get()
+                html = self.get_page_source(_url)
+                if self.browser.current_url != _url:
+                    self.visited.add(self.browser.current_url)
+                try:
+                    page = self._get_page_object(html)
+                except:
+                    page = None
+                    self.broken_urls.add(_url)
+                if page is not None:
+                    self.get_and_process_links(page, _url)
+                
+                self.visited.add(_url) 
+        except queue.Empty:
+            self.display.stop()
+            return True
+        
+
+
+
+
+
+        
+    # def get_links(self, soup):
+    #     for link in soup.find_all('a', href=True): #All links which have a href element
+    #         link = link['href'] #The actually href element of the link
+    #         url = urljoin(self.base, urldefrag(link)[0]) #Resolve relative links using base and urldefrag
+    #         if url not in self.url_queue and url not in self.crawled_urls: #Check if link is in queue or already crawled
+    #             if url.startswith(self.base): #If the URL belongs to the same domain
+    #                 self.url_queue.append(url) #Add the URL to our queue
--- tester/exceptions.py
+++ tester/exceptions.py
@@ -2,3 +2,9 @@ class ImproperlyConfigured(Exception):
     def __init__(self, message):
         _message = f'Missing parameter {message}'
         super().__init__(_message)
+
+
+class EmptyWebPage(Exception):
+    def __init__(self, url):
+        _message = f'Missing parameter {message}'
+        super().__init__(_message)
