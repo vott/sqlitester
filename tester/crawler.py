d6da245b275d72b0bd13653c61a97d3b5b369654
Victor Ottati <veoa17@gmail.com>
2018-10-16 00:13:17 +0100

    removal of debugging code

--- tester/crawler.py
+++ tester/crawler.py
@@ -70,7 +70,6 @@ class Crawler(object):
     def get_base_url(self, url):
         # Horrible 80 + LINE 2
         regex = re.compile(r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}$')
-        print('>>>' + url)
         _reg = regex.match(url)
         return(_reg.group())
 
@@ -99,7 +98,6 @@ class Crawler(object):
         pass
     
     def get_page_source(self, url):
-        print(url)
         try:
             self.browser.get(url)
             return self.browser.page_source
@@ -109,16 +107,12 @@ class Crawler(object):
     def _get_page_object(self, html):
         if not html:
             raise(EmptyWebPage)
-            print(BeautifulSoup(html, 'html'))
         return BeautifulSoup(html, 'html')
         
     def get_and_process_links(self, page, parent_url, level):
-        print('finding links')
         links = page.find_all('a', href=True)
-        print(links)
         for link in links:
             _url = link['href']
-            print(_url)
             if(self.is_relative(_url)):
                 _url = '{}{}'.format(self.get_base_url(parent_url), _url)
             if _url not in self.visited and _url not in self.broken_urls:
@@ -127,15 +121,11 @@ class Crawler(object):
     def run(self):
         try:
             level = 0
-            print(self.queue)
             while level <= self.levels:
                 item = self.queue.get()
                 _url = item[0]
                 level = item[1]
-                print(_url)
-                print(level)
                 html = self.get_page_source(_url)
-                print(html)
                 if self.browser.current_url != _url:
                     self.visited.add(self.browser.current_url)
                 try:
@@ -148,22 +138,9 @@ class Crawler(object):
                     self._process_page_object(page)
                 self.visited.add(_url) 
         except queue.Empty:
-            print(self.visited)
             self.display.stop()
             return True
         
 
 
 
-
-# if __name__=='__main__':
-#     c = Crawler(True, 'http://titan.dcs.bbk.ac.uk/~kikpef01/testpage.html', '/')
-#     c.run()
-        
-    # def get_links(self, soup):
-    #     for link in soup.find_all('a', href=True): #All links which have a href element
-    #         link = link['href'] #The actually href element of the link
-    #         url = urljoin(self.base, urldefrag(link)[0]) #Resolve relative links using base and urldefrag
-    #         if url not in self.url_queue and url not in self.crawled_urls: #Check if link is in queue or already crawled
-    #             if url.startswith(self.base): #If the URL belongs to the same domain
-    #                 self.url_queue.append(url) #Add the URL to our queue
